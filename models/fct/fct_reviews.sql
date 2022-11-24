bd75d4c40e7f9a51bebbf3594eb1d1b224ed43ab
lukpawlow <lukpawlow@gmail.com>
2022-11-24 16:49:17 +0100

    after docs

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -7,7 +7,10 @@
 with src_rev as (
   select * from {{ref("src_reviews")}}
 )
-select * from src_rev
+select
+{{ dbt_utils.surrogate_key(['LISTING_ID', 'REVIEW_DATE', 'REVIEWER_NAME', 'REVIEW_TEXT']) }} as review_id,
+* 
+from src_rev
 where src_rev.review_text is not null
 
 {% if is_incremental() %}
