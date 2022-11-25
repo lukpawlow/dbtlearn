10223a5f208decfc0d1a976a33e7dc0150d55417
lukpawlow <lukpawlow@gmail.com>
2022-11-25 15:19:00 +0100

    the end :)

---
+++ analyses/full_moon_no_sleep.sql
@@ -0,0 +1,14 @@
+with anal_mart_fullmoon_reviews as (
+    select * from {{ ref('mart_fullmoon_reviews') }}
+)
+select
+    is_full_moon,
+    review_sentiment,
+    count(*) as reviews
+from anal_mart_fullmoon_reviews
+group by
+    is_full_moon,
+    review_sentiment
+order by
+    is_full_moon,
+    review_sentiment
